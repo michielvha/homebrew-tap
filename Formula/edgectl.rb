42017f345d3a94f0d0e7b701071b69b41337d9b8
goreleaserbot <goreleaserbot@edgeforge.eu>
2025-05-12 20:46:40 +0200

    Brew formula update for edgectl version 0.3.11

--- Formula/edgectl.rb
+++ Formula/edgectl.rb
@@ -5,21 +5,21 @@
 class Edgectl < Formula
   desc "A CLI for edge-cloud"
   homepage "https://github.com/michielvha/edgectl"
-  version "0.3.10"
+  version "0.3.11"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/michielvha/edgectl/releases/download/0.3.10/edgectl_0.3.10_darwin_amd64.zip"
-      sha256 "414dc520c43b95517bf1834c757792839f523e4934a572dbe7502104725e1edd"
+      url "https://github.com/michielvha/edgectl/releases/download/0.3.11/edgectl_0.3.11_darwin_amd64.zip"
+      sha256 "8dd6c1358cc3dfb42a671294390ef0bd48373784bf090e607896421b7ee9555f"
 
       def install
         bin.install "edgectl"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/michielvha/edgectl/releases/download/0.3.10/edgectl_0.3.10_darwin_arm64.zip"
-      sha256 "414dc520c43b95517bf1834c757792839f523e4934a572dbe7502104725e1edd"
+      url "https://github.com/michielvha/edgectl/releases/download/0.3.11/edgectl_0.3.11_darwin_arm64.zip"
+      sha256 "8dd6c1358cc3dfb42a671294390ef0bd48373784bf090e607896421b7ee9555f"
 
       def install
         bin.install "edgectl"
@@ -29,15 +29,15 @@ class Edgectl < Formula
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/michielvha/edgectl/releases/download/0.3.10/edgectl_0.3.10_linux_amd64.zip"
-      sha256 "414dc520c43b95517bf1834c757792839f523e4934a572dbe7502104725e1edd"
+      url "https://github.com/michielvha/edgectl/releases/download/0.3.11/edgectl_0.3.11_linux_amd64.zip"
+      sha256 "8dd6c1358cc3dfb42a671294390ef0bd48373784bf090e607896421b7ee9555f"
       def install
         bin.install "edgectl"
       end
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/michielvha/edgectl/releases/download/0.3.10/edgectl_0.3.10_linux_arm64.zip"
-      sha256 "414dc520c43b95517bf1834c757792839f523e4934a572dbe7502104725e1edd"
+      url "https://github.com/michielvha/edgectl/releases/download/0.3.11/edgectl_0.3.11_linux_arm64.zip"
+      sha256 "8dd6c1358cc3dfb42a671294390ef0bd48373784bf090e607896421b7ee9555f"
       def install
         bin.install "edgectl"
       end
